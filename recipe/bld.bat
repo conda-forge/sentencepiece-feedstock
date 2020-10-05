004dd56771a8809c9402141bedad10e0787e9fd0
rluria14 <ryanlur14@gmail.com>
2020-10-05 12:24:16 -0400

    set mingw-64 path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,14 +5,10 @@ set LD_LIBRARY_PATH=%PREFIX%\lib
 set CPATH=%PREFIX%\include
 set INCLUDE=%PREFIX%\include
 set LIBRARY_PATH=%PREFIX%\lib
+set PATH=%PREFIX%\library\mingw-64\bin;%PATH%
 
 cmake .. -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_INSTALL_LIBDIR=%PREFIX%\lib -DSPM_ENABLE_SHARED=OFF
 cmake --build . --config Release --target install || goto :error
-
-:: make -j %CPU_COUNT%
-:: make install
-
-:: ldconfig -v -N
 
 cd %SRC_DIR%\python
 %PYTHON% setup.py build
