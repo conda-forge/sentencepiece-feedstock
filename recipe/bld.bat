c4026780c86e2f2b79dace9e48b9d4399a3694ab
oblute <olivia.blute@gmail.com>
2020-10-05 16:43:48 -0400

    build edits

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,7 +8,9 @@ if "%PLATFORM%"=="x64" set PLATFORM_PREFIX=-x64
 set _CL_=/utf-8
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 set CPATH=%PREFIX%\include
-set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
+set CURRENT_PATH=%~dp0
+set LIBRARY_PATH=%CURRENT_PATH%build\root
+set PATH=%LIBRARY_PATH%\mingw-w64\bin;%LIBRARY_PREFIX%\usr\bin;%LIBRARY_BIN%;%PATH%
 
 cmake .. -A %PLATFORM% -DSPM_BUILD_TEST=ON -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=%LIBRARY_PATH%
 cmake --build . --config Release --target install || goto :error
