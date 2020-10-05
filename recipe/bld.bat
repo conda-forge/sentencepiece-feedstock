b26dda90593a7af6a48a5fe4261f86361a174f01
oblute <olivia.blute@gmail.com>
2020-10-05 16:30:11 -0400

    new win cmake stuff

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,19 +1,18 @@
 mkdir build
 cd build
 
+set PLATFORM=%1
+if "%PLATFORM%"=="" set PLATFORM=x64
+set PLATFORM_PREFIX=
+if "%PLATFORM%"=="x64" set PLATFORM_PREFIX=-x64
+set _CL_=/utf-8
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 set CPATH=%PREFIX%\include
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 
-cmake ^
-    -D CMAKE_INSTALL_PREFIX=%PREFIX% ^
-    -DCMAKE_INSTALL_LIBDIR=%PREFIX%\lib ^
-    -DCMAKE_AR="${AR}" ^
-    -DSPM_ENABLE_TCMALLOC=OFF ^
-    -S ..
-
-make -j ${CPU_COUNT}
-make install
+cmake .. -A %PLATFORM% -DSPM_BUILD_TEST=ON -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=%LIBRARY_PATH%
+cmake --build . --config Release --target install || goto :error
+ctest -C Release || goto :error
 
 ldconfig -v -N
 cd %SRC_DIR%\python
