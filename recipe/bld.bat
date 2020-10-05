1bc391817834db83a82f2ab185847be2a3aa9db4
rluria14 <ryanlur14@gmail.com>
2020-10-05 13:00:13 -0400

    edit path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,8 @@ set LD_LIBRARY_PATH=%PREFIX%\lib
 set CPATH=%PREFIX%\include
 set INCLUDE=%PREFIX%\include
 set LIBRARY_PATH=%PREFIX%\lib
-set PATH=%PREFIX%\library\mingw-64\bin;%PATH%
+set PATH=c:\MinGW\bin;%PATH%
+set _CL_=/utf-8
 
 cmake .. -DCMAKE_INSTALL_PREFIX=%PREFIX% -DCMAKE_INSTALL_LIBDIR=%PREFIX%\lib -DSPM_ENABLE_SHARED=OFF
 cmake --build . --config Release --target install || goto :error
