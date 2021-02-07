147a659e84a90d00277252f6b14c670019ba0c7b
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-02-07 15:31:43 +0100

    Add sudo

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,12 @@ mkdir build
 cd build
 
 cmake .. -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=../..
-make -j ${CPU_COUNT}
-make install
+sudo make -j ${CPU_COUNT}
+sudo make install
 
 if [ "$(uname)" == "Linux" ];
 then
-    ldconfig -v -N
+    sudo ldconfig -v -N
 elif [ "$(uname)" == "Darwin" ];
 then
     sudo update_dyld_shared_cache
