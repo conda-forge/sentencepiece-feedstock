4ee7be34df16a19a3c61cb4bed9837beafbe080c
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-02-07 15:44:39 +0100

    Remove flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
 mkdir build
 cd build
 
-cmake .. -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=../..
-sudo make -j ${CPU_COUNT}
-sudo make install
+cmake .. -DCMAKE_INSTALL_PREFIX=../..
+make -j ${CPU_COUNT}
+make install
 
 if [ "$(uname)" == "Linux" ];
 then
