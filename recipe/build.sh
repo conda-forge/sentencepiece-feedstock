8c66baad26749caa2e9aad3a78535b4c9b875801
BastianZim <10774221+BastianZim@users.noreply.github.com>
2021-02-07 21:03:39 +0100

    Move osx section

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,26 @@
-mkdir build
-cd build
+if [[ "${target_platform}" == linux-* ]]; then
+    mkdir build
+    cd build
 
-export LD_LIBRARY_PATH=${PREFIX}/lib
-export CPATH=${PREFIX}/include
-export INCLUDE=${PREFIX}/include
-export LIBRARY_PATH=${PREFIX}/lib
+    export LD_LIBRARY_PATH=${PREFIX}/lib
+    export CPATH=${PREFIX}/include
+    export INCLUDE=${PREFIX}/include
+    export LIBRARY_PATH=${PREFIX}/lib
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
-    -DCMAKE_AR="${AR}" \
-    -DSPM_ENABLE_TCMALLOC=OFF \
-    -S ..
+    cmake \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+        -DCMAKE_AR="${AR}" \
+        -DSPM_ENABLE_TCMALLOC=OFF \
+        -S ..
 
-make -j ${CPU_COUNT}
-make install
+    make -j ${CPU_COUNT}
+    make install
 
-ldconfig -v -N
-cd $SRC_DIR/python
-${PYTHON} setup.py build
-${PYTHON} setup.py install
+    ldconfig -v -N
+    cd $SRC_DIR/python
+    ${PYTHON} setup.py build
+    ${PYTHON} setup.py install
+elif [[ "${target_platform}" == osx-* ]]; then
+      {{ PYTHON }} -m pip install python/ -vv
+fi
