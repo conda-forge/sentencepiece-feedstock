3de17c8660632efaa7a8894538315e6a2a8dca1a
H. Vetinari <h.vetinari@gmx.com>
2021-12-02 09:59:06 +1100

    improve build-lib.bat

--- recipe/build-lib.bat
+++ recipe/build-lib.bat
@@ -6,5 +6,11 @@ set LIBRARY_PATH=%CURRENT_PATH%build\root
 set PATH=%LIBRARY_PATH%\mingw-w64\bin;%LIBRARY_PREFIX%\usr\bin;%LIBRARY_BIN%;%PATH%
 set _CL_=/utf-8
 
-cmake .. -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PATH% -DSPM_ENABLE_SHARED=ON
-cmake --build . --config Release --target install || goto :error
+cmake ^
+    -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PATH% ^
+    -DSPM_ENABLE_SHARED=ON ^
+    ..
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+cmake --build . --config Release --target install
+IF %ERRORLEVEL% NEQ 0 exit 1
