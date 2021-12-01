bf1b4f5cca26e983d49d4272a97c8a0e605fc019
H. Vetinari <h.vetinari@gmx.com>
2021-12-02 00:33:01 +1100

    try to unify build-lib.sh

--- recipe/build-lib.sh
+++ recipe/build-lib.sh
@@ -1,29 +1,18 @@
 mkdir build
 cd build
 
-if [[ "${target_platform}" == linux-* ]]; then
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export CPATH=${PREFIX}/include
+export INCLUDE=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
-    export LD_LIBRARY_PATH=${PREFIX}/lib
-    export CPATH=${PREFIX}/include
-    export INCLUDE=${PREFIX}/include
-    export LIBRARY_PATH=${PREFIX}/lib
-
-    cmake \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
-        -DCMAKE_AR="${AR}" \
-        -DSPM_ENABLE_TCMALLOC=OFF \
-        -S ..
-
-elif [[ "${target_platform}" == osx-* ]]; then
-
-    cmake .. -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=../..
-    
-fi
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_AR="${AR}" \
+    -DSPM_ENABLE_TCMALLOC=OFF \
+    -S ..
 
 make -j ${CPU_COUNT}
 make install
 
-if [[ "${target_platform}" == linux-* ]]; then
-    ldconfig -v -N
-fi
+ldconfig -v -N
