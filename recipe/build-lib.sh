f9075649f37fe908e1939392ea8c4341c0fd68c2
H. Vetinari <h.vetinari@gmx.com>
2021-12-06 21:49:01 +1100

    undo unvendor of protobuf on windows and osx-arm

--- recipe/build-lib.sh
+++ recipe/build-lib.sh
@@ -6,13 +6,19 @@ export CPATH=${PREFIX}/include
 export INCLUDE=${PREFIX}/include
 export LIBRARY_PATH=${PREFIX}/lib
 
+export CMAKE_EXTRA="-DSPM_USE_BUILTIN_PROTOBUF=OFF"
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    # build calls protoc, which fails with bad CPU in cross-compilation
+    export CMAKE_EXTRA=""
+fi
+
 cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
     -DCMAKE_AR="${AR}" \
     -DSPM_ENABLE_SHARED=ON \
     -DSPM_ENABLE_TCMALLOC=OFF \
-    -DSPM_USE_BUILTIN_PROTOBUF=OFF \
+    $CMAKE_EXTRA \
     -S ..
 
 make -j ${CPU_COUNT}
