af2fcb5a83b050ec930e99d9241e0d95c413e1f7
H. Vetinari <h.vetinari@gmx.com>
2022-08-12 17:39:22 +0200

    switch to shared builds / cmake+ninja

--- recipe/build-lib.bat
+++ recipe/build-lib.bat
@@ -1,10 +1,29 @@
+@echo on
+
+:: we're trying to avoid the third_party sources, and not building them;
+:: to avoid weird errors if those sources got picked up nevertheless, delete them
+rmdir /S /Q third_party\absl
+rmdir /S /Q third_party\protobuf-lite
+
 mkdir build
 cd build
 
-set CURRENT_PATH=%~dp0
-set LIBRARY_PATH=%CURRENT_PATH%build\root
-set PATH=%LIBRARY_PATH%\mingw-w64\bin;%LIBRARY_PREFIX%\usr\bin;%LIBRARY_BIN%;%PATH%
 set _CL_=/utf-8
+set _USE_EXTERNAL_ABSL=1
+set PROTOBUF_USE_DLLS=1
+set ABSL_CONSUME_DLL=1
 
-cmake .. -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PATH% -DSPM_ENABLE_SHARED=OFF
-cmake --build . --config Release --target install || goto :error
+cmake -G "Ninja" ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%;%LIBRARY_BIN%;%LIBRARY_LIB% ^
+    -DCMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreadedDLL ^
+    -Dprotobuf_BUILD_SHARED_LIBS=ON ^
+    -DSPM_ENABLE_SHARED=ON ^
+    -DSPM_USE_BUILTIN_PROTOBUF=OFF ^
+    -DSPM_USE_EXTERNAL_ABSL=ON ^
+    ..
+IF %ERRORLEVEL% NEQ 0 exit 1
+
+cmake --build . --config Release --target install
+IF %ERRORLEVEL% NEQ 0 exit 1
