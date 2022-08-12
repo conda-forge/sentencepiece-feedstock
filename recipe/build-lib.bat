7b4f025a0fed9b7a1c26ba4113c65adb5eb086ba
H. Vetinari <h.vetinari@gmx.com>
2022-08-12 17:06:09 +0200

    break out libsentencepiece
    and incorporating lots of experience from
    https://github.com/conda-forge/sentencepiece-feedstock/pull/26

--- recipe/build-lib.bat
+++ recipe/build-lib.bat
@@ -8,9 +8,3 @@ set _CL_=/utf-8
 
 cmake .. -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PATH% -DSPM_ENABLE_SHARED=OFF
 cmake --build . --config Release --target install || goto :error
-
-cd %SRC_DIR%\python
-%PYTHON% setup.py build
-%PYTHON% setup.py install
-
-IF %ERRORLEVEL% NEQ 0 exit 1
