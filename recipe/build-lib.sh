210824928e18588c71e6ba8c73c7e6de9771c2ae
H. Vetinari <h.vetinari@gmx.com>
2022-08-12 17:39:22 +0200

    switch to shared builds / cmake+ninja

--- recipe/build-lib.sh
+++ recipe/build-lib.sh
@@ -1,28 +1,38 @@
+#!/bin/bash
+set -ex
+
+# we're trying to avoid the third_party sources, and not building them;
+# to avoid weird errors if those sources got picked up nevertheless, delete them
+rm -rf third_party/absl
+if [[ "${target_platform}" != "osx-arm64" ]]; then
+    rm -rf third_party/protobuf-lite
+    export CMAKE_EXTRA="-DSPM_USE_BUILTIN_PROTOBUF=OFF"
+else
+    # building against external protobuf needs to call protoc,
+    # which fails with bad CPU type in cross-compilation
+    export CMAKE_EXTRA=""
+fi
+
 mkdir build
 cd build
 
-if [[ "${target_platform}" == linux-* ]]; then
+export LD_LIBRARY_PATH=${PREFIX}/lib
+export CPATH=${PREFIX}/include
+export INCLUDE=${PREFIX}/include
+export LIBRARY_PATH=${PREFIX}/lib
 
-    export LD_LIBRARY_PATH=${PREFIX}/lib
-    export CPATH=${PREFIX}/include
-    export INCLUDE=${PREFIX}/include
-    export LIBRARY_PATH=${PREFIX}/lib
+cmake -G "Ninja" \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+    -DCMAKE_AR="${AR}" \
+    -DSPM_ENABLE_SHARED=ON \
+    -DSPM_ENABLE_TCMALLOC=OFF \
+    -DSPM_USE_EXTERNAL_ABSL=ON \
+    $CMAKE_EXTRA \
+    ..
 
-    cmake \
-        -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DCMAKE_INSTALL_LIBDIR=$PREFIX/lib \
-        -DCMAKE_AR="${AR}" \
-        -DSPM_ENABLE_TCMALLOC=OFF \
-        -S ..
-
-elif [[ "${target_platform}" == osx-* ]]; then
-
-    cmake .. -DSPM_ENABLE_SHARED=OFF -DCMAKE_INSTALL_PREFIX=../..
-    
-fi
-
-make -j ${CPU_COUNT}
-make install
+cmake --build .
+cmake --install .
 
 if [[ "${target_platform}" == linux-* ]]; then
     ldconfig -v -N
