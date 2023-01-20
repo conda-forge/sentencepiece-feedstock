84c01005800b94366e9efca13d3163d8aa813d86
H. Vetinari <h.vetinari@gmx.com>
2023-01-20 15:31:56 +1100

    flesh out cmake test to actually compile something

---
+++ recipe/cmake_test/test_me.cxx
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include <sentencepiece_trainer.h>
+#include <sentencepiece_processor.h>
+
+// cobbled together from https://github.com/google/sentencepiece/blob/master/doc/api.md
+int main(int argc, char** argv) {
+    sentencepiece::SentencePieceTrainer::Train("--input=../data/botchan.txt --model_prefix=m --vocab_size=1000");
+
+    sentencepiece::SentencePieceProcessor processor;
+    const auto status = processor.Load("./m.model");
+    if (!status.ok()) {
+       std::cerr << status.ToString() << std::endl;
+       return 1;
+    }
+
+    std::vector<std::string> pieces;
+    processor.Encode("This is a test.", &pieces);
+    for (const std::string &token : pieces) {
+      std::cout << token << std::endl;
+    }
+
+    return 0;
+}
